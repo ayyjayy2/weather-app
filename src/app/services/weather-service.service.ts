87540463db6fe7144866531ef078670753525e91
Alayna Johnston <55775759+ayyjayy2@users.noreply.github.com>
2024-10-15 16:31:05 -0400

    hide api key

--- src/app/services/weather-service.service.ts
+++ src/app/services/weather-service.service.ts
@@ -11,6 +11,6 @@ export class WeatherServiceService {
 
   getWeather() : Observable<any>{
     //https://api.openweathermap.org/data/2.5/weather?q={city name},{state code},{country code}&appid={API key}
-    return this.httpClient.get<any>("https://api.openweathermap.org/data/2.5/weather?q=durham,nc,us&appid=23e343a007f35ccbcc1a03ea2f52eff9"); //openweather url goes here with the city name and api key
+    return this.httpClient.get<any>("https://api.openweathermap.org/data/2.5/weather?q=durham,nc,us&appid={API Key}"); //openweather url goes here with the city name and api key
   }
 }
